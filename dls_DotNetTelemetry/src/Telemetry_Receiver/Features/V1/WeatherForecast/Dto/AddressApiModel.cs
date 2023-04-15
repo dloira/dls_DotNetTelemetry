22ed1abc53877fabef890b9e4000e3832abea037
Diego Loira <diego.loira@luckia.com>
2023-04-15 21:12:11 +0200

    DTO default values

--- dls_DotNetTelemetry/src/Telemetry_Receiver/Features/V1/WeatherForecast/Dto/AddressApiModel.cs
+++ dls_DotNetTelemetry/src/Telemetry_Receiver/Features/V1/WeatherForecast/Dto/AddressApiModel.cs
@@ -6,12 +6,12 @@ namespace Telemetry_Receiver.Features.V1.WeatherForecast.Dto
     public class AddressApiModel
     {
         [JsonProperty(PropertyName = "city")]
-        public string City { get; set; }
+        public string City { get; set; } = default!;
 
         [JsonProperty(PropertyName = "street_name")]
-        public string StreetName { get; set; }
+        public string StreetName { get; set; } = default!;
 
         [JsonProperty(PropertyName = "street_address")]
-        public string StreetAddress { get; set; }
+        public string StreetAddress { get; set; } = default!;
     }
 }
